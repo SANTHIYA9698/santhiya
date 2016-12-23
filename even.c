83c13f77d45878c601a05351ebdd10c75955ff26
SANTHIYA9698 <santhiyampp@gmail.com>
2016-12-23 01:24:52 -0800

    Add files via upload

---
+++ even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int x;
+    printf("enter the number");
+    scanf("%d",&x);
+    if((x%2)==0)
+        printf("It is even number");
+    else
+        printf("It is odd");
+
+}

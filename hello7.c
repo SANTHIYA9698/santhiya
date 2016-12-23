4d888057ad72ebfd312f8f454a1d8985a65e349d
SANTHIYA9698 <santhiyampp@gmail.com>
2016-12-23 09:44:31 -0800

    Add files via upload

---
+++ hello7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    char ch[10];
+    int i;
+    printf("enter the string");
+    scanf("%s",&ch);
+    for(i=0;i<5;i++)
+    {
+        printf("%s\n",ch);
+    }
+}

ee5ae788ea2229f094d2afc7bfaccf1bc10efe52
SANTHIYA9698 <santhiyampp@gmail.com>
2016-12-23 10:01:42 -0800

    Add files via upload

---
+++ digit10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a[100],i,digit;
+printf("Enter the integer\t");
+scanf("%d",&a);
+for(i=1;a[i]!='\0';i++)
+{
+digit=i;
+}
+printf("no of digits is %d",digit);
+}

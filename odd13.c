f246c90e88437cabc047e314f502ba98cdb211b0
SANTHIYA9698 <santhiyampp@gmail.com>
2016-12-24 07:00:01 -0800

    Add files via upload

---
+++ odd13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,n1,n2;
+printf("Enter the two values\t");
+scanf("%d %d",&n1,&n2);
+printf("\nAll odd numbers between %d and %d",n1,n2);
+for(i=n1;i<=n2;i++)
+{
+if((i%2)!=0)
+printf("\n%d",i);
+}
+return 0;
+}

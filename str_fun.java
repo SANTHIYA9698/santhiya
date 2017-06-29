0f3a44df4e68255922659522aed4d13cb7718130
M.S.SANTHIYA <santhiyampp@gmail.com>
2017-06-29 12:46:51 +0530

    Add files via upload

--- str_fun.java
+++ str_fun.java
@@ -1,25 +1,29 @@
 package logic;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class str_fun {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
-		int words=0;
+		int words=1;
 		int sentence=1;
 		int integer=0;
 		int alphe=0;
+		Pattern p=Pattern.compile("[ -/:-?[-]]*");
 		String a[]={"0","1","2","3","4","5","6","7","8","9"};
-		String str=s.nextLine();
+		String str=s.nextLine();		
 		String str1=str.trim();
 		String str2=str1.replaceAll(" ", "");
-		for(int i=0;i<str1.length();i++){
-			if((str1.substring(i,i+1).equals(" "))||(str1.substring(i,i+1).equals("."))){
+		for(int i=0;i<str1.length();i++){			
+			Matcher matcher=p.matcher(str1.substring(i,i+1));
+			if(matcher.matches()){
 				try{
-				if((!str1.substring(i+1,i+2).equals(" "))||(!str1.substring(i+1,i+2).equals("."))){
+				if(!(str1.substring(i+1,i+2).equals(" "))||(str1.substring(i+1,i+2).equals("."))){
 				words++;
-				}
+			}
 				}
 				catch(StringIndexOutOfBoundsException e){
 					System.out.println(e);
@@ -40,6 +44,7 @@ public class str_fun {
 	if(str1.substring(str1.length()-1).equals(".")){
 			System.out.println(str1.substring(str1.length()));
 			sentence=sentence-1;
+			words=words-1;
 		}
 				System.out.println("1.Spaces Removed:"+str2);
 				System.out.println("2.The longest word in the String is:"+str2);

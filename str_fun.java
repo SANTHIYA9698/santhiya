25b7744843fa674e3d479b84d571c9c5c104cb6d
M.S.SANTHIYA <santhiyampp@gmail.com>
2017-06-29 11:04:55 +0530

    Add files via upload

---
+++ str_fun.java
@@ -0,0 +1,50 @@
+package logic;
+
+import java.util.Scanner;
+
+public class str_fun {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int words=0;
+		int sentence=1;
+		int integer=0;
+		int alphe=0;
+		String a[]={"0","1","2","3","4","5","6","7","8","9"};
+		String str=s.nextLine();
+		String str1=str.trim();
+		String str2=str1.replaceAll(" ", "");
+		for(int i=0;i<str1.length();i++){
+			if((str1.substring(i,i+1).equals(" "))||(str1.substring(i,i+1).equals("."))){
+				try{
+				if((!str1.substring(i+1,i+2).equals(" "))||(!str1.substring(i+1,i+2).equals("."))){
+				words++;
+				}
+				}
+				catch(StringIndexOutOfBoundsException e){
+					System.out.println(e);
+				}
+			}
+			if(str1.substring(i,i+1).equals(".")){
+				sentence++;
+			}
+			for(int j=0;j<10;j++){
+			if(str1.substring(i,i+1).equals(a[j])){				
+				integer++;
+			}
+			}
+			if(str1.substring(i,i+1).equals("e")){				
+				alphe++;
+			}
+		}
+	if(str1.substring(str1.length()-1).equals(".")){
+			System.out.println(str1.substring(str1.length()));
+			sentence=sentence-1;
+		}
+				System.out.println("1.Spaces Removed:"+str2);
+				System.out.println("2.The longest word in the String is:"+str2);
+				System.out.println("3.No.of letters e in the String is:"+alphe+"\n4.No.of integers in the String is:"+integer+"\n6.no.of words in the String is:"+words);
+				System.out.println("7.No.of sentence in the String is:"+sentence);
+				s.close();
+	}
+}

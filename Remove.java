033bf9085b62a41f541c3cfc4ecc6f1d1497a33f
M.S.SANTHIYA <santhiyampp@gmail.com>
2017-07-04 14:44:00 +0530

    Add files via upload

--- Remove.java
+++ Remove.java
@@ -1,25 +1,28 @@
+package logic;
+
 import java.util.Scanner;
-class Remove{
-public static void main(String []args){
-Scanner s=new Scanner(System.in);
-System.out.println("Enter the no of elements in array:");
-int n=s.nextInt();
-System.out.println("Enter the elements in the array:");
-int arr[]=new int[n];
+
+public class Remove {
+
+	public static void main(String[] args) {
+   Scanner s=new Scanner(System.in);
+   System.out.println("Enter the number Of elements in array:");
+   int n=s.nextInt();
+   System.out.println("Enter the elements:");
+   int arr[]=new int[n];
 for(int i=0;i<n;i++){
-arr[i]=s.nextInt();
+	arr[i]=s.nextInt();
 }
-System.out.println("Enter the element to boe removed:");
+System.out.println("Enter the element to be removed:");
 int element=s.nextInt();
 for(int i=0;i<n;i++){
-if(arr[i]==element){
-arr[i]=arr[i+1];
-}
-n--;
-}
+	if(arr[i]==element){
+		arr[i]=arr[i+1];
+	}
+	n--;
+	}
 for(int i=0;i<n;i++){
-System.out.print(arr[i]+" ");
-}
-s.close();
+System.out.println(arr[i]);
+	}
 }
 }

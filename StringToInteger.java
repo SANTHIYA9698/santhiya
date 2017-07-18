357be9ca9baabe1ce6df039be774cbc2d7a619d3
M.S.SANTHIYA <santhiyampp@gmail.com>
2017-07-18 21:01:47 +0530

    Add files via upload

---
+++ StringToInteger.java
@@ -0,0 +1,10 @@
+package guvi;
+public class StringToInteger {
+	
+	public static void main(String[] args) {
+String input="1413093";
+int output=Integer.parseInt(input);
+System.out.println("Converted integer is:"+output);
+	}
+
+}

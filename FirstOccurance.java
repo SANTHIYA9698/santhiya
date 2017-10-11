a9df46e875e882dbf32b42c7370e6effb233c00d
M.S.SANTHIYA <santhiyampp@gmail.com>
2017-10-11 21:03:50 +0530

    Update FirstOccurance.java

--- FirstOccurance.java
+++ FirstOccurance.java
@@ -1,9 +1,15 @@
 package Hunter;
-
+import java.util.*;
 public class FirstOccurance {
 
 	public static void main(String[] args) {
-String input[]={"AB","CD","E","AN"};
+//String input[]={"AB","CD","E","AN"};
+Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+String input[]=new String[n];	
+		for(int i=0;i<n;i++){
+			input[i]=s.next();
+		}
 int length=999999999;
 String output="";
 for(int i=0;i<input.length;i++){

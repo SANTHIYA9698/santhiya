f4da8e633fc591121ff024225493f85bd7f43214
M.S.SANTHIYA <santhiyampp@gmail.com>
2017-10-24 12:25:16 +0530

    Update StringMultiplication.java

--- StringMultiplication.java
+++ StringMultiplication.java
@@ -1,16 +1,18 @@
 package Hunter;
-
+import java.util.*;
 public class StringMultiplication {
 
 	public static void main(String[] args) {
-String number1="40";
-String number2="50";
+		Scanner s=new Scanner(System.in);
+String number1=s.next();
+String number2=s.next();
 try{
 String output=Integer.toString(Integer.parseInt(number1)*Integer.parseInt(number2));
 System.out.println(output);
 }catch(NumberFormatException e){
 	
 }
+		s.close();
 	}
 
 }

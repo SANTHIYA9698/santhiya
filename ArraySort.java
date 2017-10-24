2fab64ae5588338c81e8647234c0b96581cacfc5
M.S.SANTHIYA <santhiyampp@gmail.com>
2017-10-24 10:27:45 +0530

    Update ArraySort.java

--- ArraySort.java
+++ ArraySort.java
@@ -5,20 +5,14 @@ public class ArraySort {
 	public static void main(String[] args) {
 Scanner s=new Scanner(System.in);
 int mark[]=new int[50];
-int mark1[]=new int[50];
-for(int i=0;i<10;i++){
+for(int i=0;i<50;i++){
 	mark[i]=s.nextInt();
 }
 Arrays.sort(mark);
-for(int i=9,j=0;i>=0;i--,j++){
-mark1[j]=mark[i];	
-}
-for(int i=0;i<10;i++){
-	System.out.print(mark1[i]+" ");
-}
 int k=s.nextInt();
-System.out.println("\n 4th tallest:"+mark1[3]);
-System.out.println("kth tallest:"+mark1[k-1]);
+System.out.println("\n 4th tallest:"+mark[50-4]);
+System.out.println("kth tallest:"+mark1[50-k]);
+		s.close();
 	}
 
 }
